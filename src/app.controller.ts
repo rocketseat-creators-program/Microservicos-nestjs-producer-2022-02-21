ba9d3a78eecfd26deab9be48334663c39b88d593
brenoos <breno.oliver07@gmail.com>
2022-02-22 11:25:46 -0300

    finish class

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  Client,
+  ClientKafka,
+  ClientRMQ,
+  Transport,
+} from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Client({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'storage-ec',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  })
+  client: ClientRMQ;
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  sendToStorage() {
+    return this.client.send('storage-ec', {
+      message: 'remove chicken from storage',
+    });
   }
 }
